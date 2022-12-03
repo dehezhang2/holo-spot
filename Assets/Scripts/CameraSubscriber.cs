c23836459007df5aac678a567537dbfc394623f2
Ganlin Zhang <zgl0315@gmail.com>
2022-12-03 09:27:15 +0000

    rotate image plane

--- Assets/Scripts/CameraSubscriber.cs
+++ Assets/Scripts/CameraSubscriber.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using CompressedImage = RosMessageTypes.Sensor.CompressedImageMsg;
 using Image = RosMessageTypes.Sensor.ImageMsg;
 using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.Sensor;
+using System;
 
 public class CameraSubscriber : MonoBehaviour
 {
@@ -15,12 +17,14 @@ public class CameraSubscriber : MonoBehaviour
     // Start is called before the first frame update
     private Vector3 localPos = new Vector3(0.22f, 0.1f, 1f);
     public Quaternion localRotation = Quaternion.Euler(90, 180, 0);
-    public float rotate;
-    public Vector3 axis;
+    private float currentImgAngle = 0;
+    public float angleToRotate = 0;
+    public bool isRotateMsgReceived;
     void Start()
     {   
         ros = ROSConnection.GetOrCreateInstance();
         ros.Subscribe<CompressedImage>("/spot/camera/hand_color/compressed", ShowImage);
+        ros.Subscribe<JointStateMsg>("/joint_states", ChangeImagePlaneAngle);
         texture2D = new Texture2D(640, 480);
         gameObject.GetComponent<Renderer>().material = new Material(Shader.Find("Standard"));
         
@@ -34,7 +38,13 @@ public class CameraSubscriber : MonoBehaviour
             ProcessImage();
         gameObject.transform.localPosition = localPos;
         gameObject.transform.localRotation = localRotation;
-        gameObject.transform.Rotate(axis.x, axis.y, axis.z, Space.Self);
+        if (isRotateMsgReceived) {
+            gameObject.transform.Rotate(0, angleToRotate, 0, Space.Self);
+            localRotation = gameObject.transform.localRotation;
+            currentImgAngle = currentImgAngle + angleToRotate;
+            isRotateMsgReceived = false;
+        }
+        
     }
 
     void ShowImage(CompressedImage ImgMsg)
@@ -49,7 +59,15 @@ public class CameraSubscriber : MonoBehaviour
         texture2D.LoadImage(imageData);
         texture2D.Apply();
         gameObject.GetComponent<Renderer>().material.mainTexture = texture2D;
-        //gameObject.GetComponent<Renderer>().material.SetTexture("_MainTex", texture2D);
         isMessageReceived = false;
+    }
+
+    void ChangeImagePlaneAngle(JointStateMsg jointState) {
+        float handAngle = (float) (jointState.position[jointState.position.Length - 2] * 180.0 / Math.PI) ;
+        angleToRotate = handAngle - currentImgAngle;
+        if (angleToRotate > 3) {
+            isRotateMsgReceived = true;
+        }
+        
     }
 }
