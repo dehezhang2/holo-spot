afd2d885d501d181ad0749724be74be9796c61f3
VpMV-EjtnPozpLGE <111972284+VpMV-EjtnPozpLGE@users.noreply.github.com>
2022-12-10 12:46:45 +0100

    Help Panel Updated

--- Assets/Scripts/CommandPanel.cs
+++ Assets/Scripts/CommandPanel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CommandPanel : MonoBehaviour
 {
-    private Vector3 localPos = new Vector3(0.0f, 0.5f, 1.2f);
+    private Vector3 localPos = new Vector3(-0.5f, 0.25f, 2.5f);
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +12,20 @@ public class CommandPanel : MonoBehaviour
     }
 
     // Update is called once per frame
+
     void Update()
     {
+        //gameObject.transform.localPosition = localPos;
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("PrintOnDisable: script was disabled");
+    }
+
+    void OnEnable()
+    {
         gameObject.transform.localPosition = localPos;
+        Debug.Log("PrintOnEnable: script was enabled");
     }
 }
